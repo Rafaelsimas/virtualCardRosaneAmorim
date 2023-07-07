fa72e5d2f8f9667cc0dc0602f3eef6653051d4b3
Rafael Simas <rafael.simas@al.resilia.com.br>
2023-07-07 11:30:00 -0300

    Primeira versão do virtualCard

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const openPixKey = () => {
+  const openCloseCard  = document.querySelector('.open-close-card ')
+  openCloseCard.classList.remove('escondido')
+}
+
+const btnCloseCardPix = () => {
+  const openCloseCard  = document.querySelector('.open-close-card ')
+  openCloseCard.classList.add('escondido')
+}
